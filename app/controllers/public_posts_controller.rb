dad118df6a11674db4302b0522901789a7eb0ccf
Cesario Silva <silva@cfsilva.com>
2025-08-29 09:51:14 +0200

    fixed width on mobile posts

--- app/controllers/public_posts_controller.rb
+++ app/controllers/public_posts_controller.rb
@@ -4,6 +4,14 @@ class PublicPostsController < ApplicationController
     @user = User.find_by!(username: params[:username].downcase)
     @post = @user.posts.published.friendly.find(params[:id])
 
+    # Fetch more posts
+
+    @more_posts = @user.posts
+                    .where.not(id: @post.id)
+                    .published
+                    .order(published_at: :desc)
+                    .limit(2)
+
     track_post_view!(@post)
 
     render :show
